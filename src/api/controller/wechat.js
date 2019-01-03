cd09408b28ec2df82148a35261303fa72ac363f5
baisheng <baisheng@gmail.com>
2019-01-03 23:24:24 +0800

    更新ai 识别逻辑

--- src/api/controller/wechat.js
+++ src/api/controller/wechat.js
@@ -286,8 +286,8 @@ module.exports = class extends Base {
           secret: this.appSecret
         });
         const res = await aiService.image(base64Data)
-        console.log('开启百度云识别。。。。。。。。。')
-        console.log(res)
+        // console.log('开启百度云识别。。。。。。。。。')
+        // console.log(res)
         if (res.result_num > 0) {
           for (let item of res.result) {
             if (item.keyword.includes('米') || item.root.includes('食品') || item.root.includes('食物')) {
