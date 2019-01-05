1127a7cf29463953be87fc7e481ddd088d18107e
baisheng <baisheng@gmail.com>
2019-01-05 20:06:51 +0800

    update

--- src/api/controller/coupon.js
+++ src/api/controller/coupon.js
@@ -8,7 +8,7 @@ const FormData = require('form-data');
  * 优惠券 API
  * @type {module.exports}
  */
-mdule.exports = class extends Base {
+module.exports = class extends Base {
   constructor (ctx) {
     super(ctx); // 调用父级的 constructor 方法，并把 ctx 传递进去
     // 其他额外的操作
