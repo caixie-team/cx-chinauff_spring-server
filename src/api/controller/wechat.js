35a012cac2058671887a0960f0f5c89d2c0fcde9
baisheng <baisheng@gmail.com>
2019-01-06 00:42:22 +0800

    update page261

--- src/api/controller/wechat.js
+++ src/api/controller/wechat.js
@@ -210,7 +210,7 @@ module.exports = class extends Base {
 
   async getAccessToken () {
     const query = queryString.stringify({
-      appid: 'wxa8299eb7fc27ef04'
+      appid: 'wxb44ce8b8c5cfdc0a'
     })
     console.log('REQUEST ACCESS TOKEN...')
     console.log('REQUEST PARMS...')
