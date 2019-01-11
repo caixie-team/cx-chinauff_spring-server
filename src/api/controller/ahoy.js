9fa3021dfb1a8e20efb5d7e3c60df4b5e1c310f2
baisheng <baisheng@gmail.com>
2019-01-11 23:24:15 +0800

    fix ahoy

--- src/api/controller/ahoy.js
+++ src/api/controller/ahoy.js
@@ -2,7 +2,6 @@
 // import speakeasy from 'speakeasy';
 const jwt = require('jsonwebtoken')
 const Base = require('./base')
-
 module.exports = class extends Base {
   /**
    * 用户访问
@@ -30,21 +29,22 @@ module.exports = class extends Base {
     }).find()
     // console.log(visitor)
     const eventModel = this.model('ahoy_event')
-    const eventData = JSON.parse(data.events_json)[0]
-    const properties = eventData.properties
-    // console.log(properties)
-    await eventModel.add({
-      visit_token: data.visit_token,
-      visitor_token: data.visitor_token,
-      name: eventData.name,
-      visit_id: visitor.id,
-      properties: JSON.stringify(eventData.properties),
-      time: eventData.time
-    })
-
-    const analyticsModel = this.model('analytics')
-    const dayView = await analyticsModel.getDayPageView()
-    const allView = await analyticsModel.getAllPageView()
+    if (think._.has(data, 'event_json')) {
+      const eventData = JSON.parse(data.events_json)[0]
+      const properties = eventData.properties
+      // console.log(properties)
+      await eventModel.add({
+        visit_token: data.visit_token,
+        visitor_token: data.visitor_token,
+        name: eventData.name,
+        visit_id: visitor.id,
+        properties: JSON.stringify(eventData.properties),
+        time: eventData.time
+      })
+    }
+    // const analyticsModel = this.model('analytics')
+    // const dayView = await analyticsModel.getDayPageView()
+    // const allView = await analyticsModel.getAllPageView()
     // console.log('---- day view ----')
     // console.log(dayView)
     // console.log('---- alll view ----')
